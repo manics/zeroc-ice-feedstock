eaa45eb1921206dbb5b1f8a8df1839c7c168b1d3
Simon Li <orpheus+devel@gmail.com>
2022-04-05 19:51:31 +0100

    OSX: set ARCHFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,8 @@ export CPP_INCLUDE_PATH=$PREFIX/include
 export CXX_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include
 export LD_LIBRARY_PATH=$PREFIX/lib
+if [ -n "$OSX_ARCH" ]; then
+  # https://github.com/zeroc-ice/ice-packaging/blob/v3.6.5/ice/pypi/setup.py#L49-L51
+  export ARCHFLAGS="-arch $OSX_ARCH"
+fi
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
